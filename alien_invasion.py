b297fce94ea4e7f2499aecb22dc4f2dfd0cc1423
GuillermoLenovo <gerardobec35@gmail.com>
2020-09-12 12:26:20 -0500

    Creamos un objeto Settings para toda las partes de configuracion del juego, separando asi las tareas

--- alien_invasion.py
+++ alien_invasion.py
@@ -10,7 +10,8 @@ class AlienInvasion:
         pygame.init() # Crea los componentes de pygame para que funcione apropiadamente.
         self.settings = Settings() # crea una instancia de la Clase Settings.
 
-        self.screen = pygame.display.set_mode( (1200, 800) ) # Devuelve un objeto called surface que representa un componente en la pantalla, de tamaño 1200x800.
+        self.screen = pygame.display.set_mode(
+                (self.settings.screen_width, self.settings.screen_height) ) # Devuelve un objeto called surface que representa un componente en la pantalla, de tamaño 1200x800.
         pygame.display.set_caption("Alien Invasion") # Ajusta el title
 
 
@@ -22,6 +23,9 @@ class AlienInvasion:
             for event in pygame.event.get(): # Devuelve una lista de eventos generada cada vez que entra al loop.
                 if event.type == pygame.QUIT: # Evento generado cuando se cierra la pantalla
                     sys,exit() # Termina la ejecucion del programa.
+
+            # Redraw the screen during each pass through the looṕ
+            self.screen.fill(self.settings.bg_color)
 
             # Make the most recently draw screen visible.
             pygame.display.flip() # udpate screen and borrow old screen.
